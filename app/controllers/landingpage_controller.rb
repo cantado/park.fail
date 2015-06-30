4338201fd9413ababc0407a665ffa13df1921bca
Stefan Laimer <stlaimer@gmail.com>
2015-06-30 13:15:39 +0200

    add gem 'haml' to project and create landingpage for root view

---
+++ app/controllers/landingpage_controller.rb
@@ -0,0 +1,4 @@
+class LandingpageController < ActionController::Base
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # devise auto generate
   devise_for :users
 
-  # set root directory
-  root to: 'home#index'
+  authenticated do
+    root to: 'park#index', as: :authenticated
+  end
+
+  root to: 'landingpage#index'
 
 end
